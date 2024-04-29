a0d20fbf5359eae3e2a5e737aeff6deecea22ad7
MauricioMunguia10 <mauriciomunguiagarcia@gmail.com>
2024-04-29 17:19:29 -0600

    ejercicio terminado

--- projects/hola_mundo/src/App1.jsx
+++ projects/hola_mundo/src/App1.jsx
@@ -2,11 +2,44 @@ import './App.css'
 //siempre importar la función a mostrar
 import { TwitterFollowCard } from './TwitterFollowCard'
 
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Angel',
+        isFollowing: true
+    },
+    {
+        userName: 'mauricio',
+        name: 'Mauricio M ',
+        isFollowing: true
+    },
+    {
+        userName: 'angel',
+        name: 'Angel G',
+        isFollowing: false
+    },
+]
 export function App () {
-    return( 
-     <>
-        <TwitterFollowCard userName="mau10" name="Mauricio Munguia"/>
-        <TwitterFollowCard userName="midudev" name="Miguel Angel"/>
-     </>
+    //funcion que agrega el arroba a cada usuario
+    const format = (userName) => `@${userName}`
+
+    return(
+        //tarjetas de Twiter 
+     <section className='App'>
+        
+        {
+        users.map(user => {
+            const {userName, name, isFollowing} = user
+            return(
+                <TwitterFollowCard
+                key={userName} //tiene que ser un identificar unico
+                userName={userName}
+                initialIsFollowing={isFollowing}
+                > {name} </TwitterFollowCard>
+            )
+        })
+        }
+
+     </section>
     )
 }
